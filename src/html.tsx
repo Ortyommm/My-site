069d72736b13250c68d718d45ec3c7d6f126ca12
Ortyommm <artemdemargaryan@gmail.com>
2024-09-01 16:59:44 +0300

    html.tsx metrika fix

--- src/html.tsx
+++ src/html.tsx
@@ -12,8 +12,7 @@ export default function HTML(props: propTypes) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {/* Yandex.Metrika counter */}
-        <script type="text/javascript" >
-          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        <script type="text/javascript" dangerouslySetInnerHTML={{__html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
           m[i].l=1*new Date();
           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
           k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
@@ -23,9 +22,9 @@ export default function HTML(props: propTypes) {
           clickmap:true,
           trackLinks:true,
           accurateTrackBounce:true
-        });
+        });`}}>
         </script>
-        <noscript><div><img src="https://mc.yandex.ru/watch/98214577" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <noscript><div><img src="https://mc.yandex.ru/watch/98214577" style={{position: 'absolute', left:-9999}} alt="" /></div></noscript>
         {/* /Yandex.Metrika counter */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
